e9adc40604d7b7f351a4786c2ab7bb5a996c8493
Moon YuBin <yubin5744@gmail.com>
2025-10-23 22:49:46 +0900

    fix : add bookController API to WHITE_LIST

--- src/main/java/com/example/quotes/common/consts/Const.java
+++ src/main/java/com/example/quotes/common/consts/Const.java
@@ -6,6 +6,7 @@ public interface Const {
     //TODO : 추후 로그인 없이도 방문가능한 페이지의 경우 해당 화이트 리스트에 URL 추가
     String[] WHITE_LIST = {
             "/signup",
-            "/signin"
+            "/signin",
+            "/books"
     };
 }
