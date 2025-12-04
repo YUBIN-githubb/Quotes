304478a1b7698ad072bb7a4c6f9b9cb9e9fdf294
Moon YuBin <yubin5744@gmail.com>
2025-12-04 21:49:06 +0900

    fix : 좋아요 API 응답 수정

--- src/main/java/com/example/quotes/domain/like/dto/response/LikeResponse.java
+++ src/main/java/com/example/quotes/domain/like/dto/response/LikeResponse.java
@@ -1,22 +1,76 @@
 package com.example.quotes.domain.like.dto.response;
 
 import com.example.quotes.common.enums.Category;
+import com.example.quotes.common.enums.IsPublic;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class LikeResponse {
 
+    private final Long id;
     private final Long quoteId;
+    private final Long userId;
+    private final String nickname;
     private final String title;
+    private final String author;
     private final Category category;
+    private final Long pageNumber;
+    private final String sentence;
+    private final String thought;
+    private final IsPublic isPublic;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+    private final LocalDateTime deletedAt;
 
-    private LikeResponse(Long quoteId, String title, Category category) {
+    private LikeResponse(
+            Long id,
+            Long quoteId,
+            Long userId,
+            String nickname,
+            String title,
+            String author,
+            Category category,
+            Long pageNumber,
+            String sentence,
+            String thought,
+            IsPublic isPublic,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt,
+            LocalDateTime deletedAt
+    ) {
+        this.id = id;
         this.quoteId = quoteId;
+        this.userId = userId;
+        this.nickname = nickname;
         this.title = title;
+        this.author = author;
         this.category = category;
+        this.pageNumber = pageNumber;
+        this.sentence = sentence;
+        this.thought = thought;
+        this.isPublic = isPublic;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.deletedAt = deletedAt;
     }
 
-    public static LikeResponse of(Long quoteId, String title, Category category) {
-        return new LikeResponse(quoteId, title, category);
+    public static LikeResponse of(
+            Long id,
+            Long quoteId,
+            Long userId,
+            String nickname,
+            String title,
+            String author,
+            Category category,
+            Long pageNumber,
+            String sentence,
+            String thought,
+            IsPublic isPublic,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt,
+            LocalDateTime deletedAt) {
+        return new LikeResponse(id, quoteId, userId, nickname, title, author, category, pageNumber, sentence, thought, isPublic, createdAt, modifiedAt, deletedAt);
     }
 }
--- src/main/java/com/example/quotes/domain/like/dto/response/PageLikeResponse.java
+++ src/main/java/com/example/quotes/domain/like/dto/response/PageLikeResponse.java
@@ -32,10 +32,23 @@ public class PageLikeResponse {
     public static PageLikeResponse of(List<Like> content, int size, int page, long totalElements, int totalPages) {
         List<LikeResponse> modifiedContent = content.stream().map(
                 like -> {
+                    Long id = like.getId();
                     Long quoteId = like.getQuote().getId();
+                    Long userId = like.getUser().getId();
+                    String nickname = like.getUser().getNickname();
                     String title = like.getQuote().getTitle();
+                    String author = like.getQuote().getAuthor();
                     Category category = like.getQuote().getCategory();
-                    return LikeResponse.of(quoteId, title, category);
+                    Long pageNumber = like.getQuote().getPageNumber();
+                    String sentence = like.getQuote().getSentence();
+                    String thought = like.getQuote().getThought();
+                    IsPublic isPublic = like.getQuote().getIsPublic();
+                    LocalDateTime createdAt = like.getQuote().getCreatedAt();
+                    LocalDateTime modifiedAt = like.getQuote().getModifiedAt();
+                    LocalDateTime deletedAt = like.getQuote().getDeletedAt();
+                    return LikeResponse.of(
+                            id, quoteId, userId, nickname, title, author, category, pageNumber, sentence, thought, isPublic, createdAt, modifiedAt, deletedAt
+                    );
                 }
         ).toList();
         return new PageLikeResponse(modifiedContent, size, page, totalElements, totalPages);
